388419fb2dba14be9317cd567e38c488820cca6a
Amogo Solomon <amogoagaba@gmail.com>
2019-12-19 05:08:57 +0100

    String value on station statistics was edited

--- bikeshare.py
+++ bikeshare.py
@@ -116,7 +116,8 @@ def time_stats(df):
 
 
 def station_stats(df):
-    """Displays statistics on the most popular stations and trip."""
+    #"""Displays statistics on the most popular stations and trip."""
+    """Displaying Statistics on the most popular stations and Trip"""
 
     print('\nCalculating The Most Popular Stations and Trip...\n')
     start_time = time.time()
