5edffcca6c857466e08129bd2a97533d410539b7
Amogo Solomon <amogoagaba@gmail.com>
2019-12-19 04:54:43 +0100

    time-stat title was edited in bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -92,7 +92,7 @@ def load_data(city, month, day):
 
 def time_stats(df):
     #"""Displays statistics on the most frequent times of travel."""
-    """Displaying mode of the statistics on frequency of travel"""
+    """Displaying mode of the statistics on frequency of time travel"""
 
     print('\nCalculating The Most Frequent Times of Travel...\n')
     start_time = time.time()
