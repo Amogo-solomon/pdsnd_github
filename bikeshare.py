991dd2b917d4e2ccb0bbe15d28481d311627d65d
Amogo Solomon <amogoagaba@gmail.com>
2019-12-19 05:16:06 +0100

    Display message on trip_duration was edited

--- bikeshare.py
+++ bikeshare.py
@@ -143,7 +143,8 @@ def station_stats(df):
 
 
 def trip_duration_stats(df):
-    """Displays statistics on the total and average trip duration."""
+    #"""Displays statistics on the total and average trip duration."""
+    """Displaying statistics on the total and average trip duration"""
 
     print('\nCalculating Trip Duration...\n')
     start_time = time.time()
